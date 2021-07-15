e9dd030af10f13652eaa1ac223c203d247178e46
lucky935 <84118606+lucky935@users.noreply.github.com>
2021-07-15 23:21:29 +0530

    Create create  window.py

---
+++ create  window.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()  #creates a blank window
+
+my_Lable=Label(root,text="yoo wassup")  #creating a label widget
+
+my_Lable.pack() #placing widget indside the window
+
+root.mainloop()# keep window open,close button breaks the loop
+
